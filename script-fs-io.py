72815955b6b2081ae3d14f725fa0ab0404dbce67
Ehsan Mohammadi <ehsanx64@gmail.com>
2020-05-25 15:25:14 +0430

    Code reformat at script-fs-io.py

--- script-fs-io.py
+++ script-fs-io.py
@@ -6,48 +6,55 @@
 
 import sys
 
+
 # Wait for user to press Enter key
 def wait_for_enter():
-	a = input("Press Enter to continue...")
+    a = input("Press Enter to continue...")
+
 
 # Greet the given name
 def say_hello(name):
-	print("Hello dear " + name)
+    print("Hello dear " + name)
+
 
 # Greet the given name, nothing printed result returned
 def get_say_hello(name):
-	return "Hello dear " + name
+    return "Hello dear " + name
+
 
 # Write content to given filename
 def write_data(filename, content):
-	outfile = filename
-	fout = open(outfile, "w")
-	fout.write(content)
-	fout.close()
+    outfile = filename
+    fout = open(outfile, "w")
+    fout.write(content)
+    fout.close()
+
 
 # Program entry
 def main():
-	# For debugging ...
-	#print(sys.argv)
-	#print(len(sys.argv))
-	#print(sys.argv[0])
+    # For debugging ...
+    # print(sys.argv)
+    # print(len(sys.argv))
+    # print(sys.argv[0])
 
-	# Program must not continue unless there are three arguments
-	if len(sys.argv) != 3:
-		print("Invalid arguments")
-		exit()
-	
-	# First argument is the python script (current file)
-	# Second argument is input data file path
-	print("Arguments file: " + sys.argv[1])
+    # Program must not continue unless there are three arguments
+    if len(sys.argv) != 3:
+        print("Invalid arguments")
+        exit()
 
-	# Open the input file and read its content (stripping whitespace chars)
-	fin = open(sys.argv[1], "r")
-	text = fin.readlines()
-	theName = text[0].strip()
+    # First argument is the python script (current file)
+    # Second argument is input data file path
+    print("Arguments file: " + sys.argv[1])
 
-	# Write the outfile data to output file path
-	write_data(sys.argv[2], get_say_hello(theName))
-	#wait_for_enter()
+    # Open the input file and read its content (stripping whitespace chars)
+    fin = open(sys.argv[1], "r")
+    text = fin.readlines()
+    theName = text[0].strip()
+
+    # Write the outfile data to output file path
+    write_data(sys.argv[2], get_say_hello(theName))
+
+
+# wait_for_enter()
 
 main()
